569e223479c78334bf3cdfb0c160b3c20bc24e4e
Maksim Kalevich <maksim_kalevich@epam.com>
2024-01-26 21:59:57 +0300

    node-nodejs-basics: Complete CLI module

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,15 @@
 const parseEnv = () => {
-    // Write your code here 
+  const { stdout } = process;
+
+  for (const key in process.env) {
+    const targetPrefix = "RSS_";
+
+    if (key.startsWith(targetPrefix)) {
+      const formattedString = `${key}=${process.env[key]}\n`;
+
+      stdout.write(formattedString);
+    }
+  }
 };
 
 parseEnv();
