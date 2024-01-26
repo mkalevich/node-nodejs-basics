e17f0b438e62499ba6578bcea1d44fa38db6545e
Maksim Kalevich <maksim_kalevich@epam.com>
2024-01-27 00:27:41 +0300

    node-nodejs-basics: Complete Hash module

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,31 @@
+import crypto from "crypto";
+import path from "path";
+import { fileURLToPath } from "url";
+import fs from "fs";
+
 const calculateHash = async () => {
-    // Write your code here 
+  const hash = crypto.createHash("sha256");
+
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+
+  const pathToTargetFile = path.join(
+    __dirname,
+    "files",
+    "fileToCalculateHashFor.txt",
+  );
+
+  const readableStream = fs.createReadStream(pathToTargetFile);
+
+  readableStream.on("readable", () => {
+    const data = readableStream.read();
+
+    if (data) {
+      hash.update(data);
+    } else {
+      console.log(hash.digest("hex"));
+    }
+  });
 };
 
 await calculateHash();
