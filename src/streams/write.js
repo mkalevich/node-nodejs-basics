df37172b0406621bac551dedf046225268a4cfc8
Maksim Kalevich <maksim_kalevich@epam.com>
2024-01-27 00:58:28 +0300

    node-nodejs-basics: Complete Streams 2/3 module

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,22 @@
+import path from "path";
+import { fileURLToPath } from "url";
+import fs from "fs";
+
 const write = async () => {
-    // Write your code here 
+  const { stdin } = process;
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const targetFile = path.join(__dirname, "files", "fileToWrite.txt");
+
+  const writableStream = fs.createWriteStream(targetFile);
+
+  stdin.on("data", (chunk) => {
+    writableStream.write(chunk);
+  });
+
+  stdin.on("end", () => {
+    writableStream.end();
+  });
 };
 
 await write();
