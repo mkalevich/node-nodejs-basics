40ce369036e96453afb449ac54aa79b6390d8c09
Maksim Kalevich <maksim_kalevich@epam.com>
2024-01-27 19:08:01 +0300

    node-nodejs-basics: Complete Streams 3/3 module

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,17 @@
+import { Transform } from "stream";
+
 const transform = async () => {
-    // Write your code here 
+  const { stdin, stdout } = process;
+
+  const reverseTextTransformStream = new Transform({
+    transform(chunk, encoding, callback) {
+      const reversedChunk = chunk.toString().split("").reverse().join("");
+
+      callback(null, reversedChunk);
+    },
+  });
+
+  stdin.pipe(reverseTextTransformStream).pipe(stdout);
 };
 
 await transform();
