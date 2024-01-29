4d9f5d6f43118979e16c48a685fd7df9bcfb98de
Maksim Kalevich <maksim_kalevich@epam.com>
2024-01-29 19:58:39 +0300

    node-nodejs-basics: Complete Zlib module

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,21 @@
+import zlib from "zlib";
+import { fileURLToPath } from "url";
+import path from "path";
+import fs from "fs";
+
 const compress = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+
+  const fileToCompress = path.join(__dirname, "files", "fileToCompress.txt");
+
+  const gzip = zlib.createGzip();
+  const input = fs.createReadStream(fileToCompress);
+  const output = fs.createWriteStream(
+    path.join(__dirname, "files", "archive.gz"),
+  );
+
+  input.pipe(gzip).pipe(output);
 };
 
 await compress();
